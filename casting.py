6b3c73492e6581ef96c4035aa4e1bbe58ab9fbf7
joaquindevoto <jdevoto08@gmail.com>
2026-03-18 18:42:14 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -9,7 +9,7 @@ def casting():
     precio_descuento=(float(precio-descuento))
     total=float(precio_descuento*cantidad)
 
-    print(f"Precio:{precio}")
-    print(f"Descuento:{descuento}")
-    print(f"Precio con descuento:{precio_descuento}")
-    print(f"Total:{total}")
+    print(f"Precio: {precio}")
+    print(f"Descuento: {descuento}")
+    print(f"Precio con descuento: {precio_descuento}")
+    print(f"Total: {total}")
