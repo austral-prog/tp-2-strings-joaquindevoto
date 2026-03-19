74cd00267a53a01eec06e7b66968418ac99bb55f
joaquindevoto <jdevoto08@gmail.com>
2026-03-19 19:47:11 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,7 +2,6 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
       nombre=input("Ingrese si nombre: ")
     apellido=input("Ingrese su apellido: ")
     nombre_completo=(nombre+" " +apellido)
